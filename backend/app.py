af9f1404e526c3273b462a79d61fe0a76b776d5a
asphodel42 <asphodell42@gmail.com>
2025-07-22 19:10:57 +0300

    init project

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='../frontend/templates',
+            static_folder='../frontend/static')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ frontend/static/js/app.js
@@ -0,0 +1,3 @@
+const Heading = document.querySelector(".page h2");
+
+console.log(`Title: ${Heading.innerText}`);
