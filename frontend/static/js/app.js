92491c4e477027758b4fbc8e75cf7e68472395e9
asphodel42 <asphodell42@gmail.com>
2025-07-23 18:50:40 +0300

    Implement plot page layout and test functionality with responsive design

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -5,21 +5,3 @@ BurgerBtn.addEventListener("click", () => {
   BurgerBtn.classList.toggle("active");
   Nav.classList.toggle("active");
 });
-
-TESTER = document.getElementById("tester");
-
-Plotly.newPlot(
-  TESTER,
-  [
-    {
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16],
-    },
-  ],
-  {
-    margin: { t: 0 },
-  },
-  { showSendToCloud: true }
-);
-
-console.log(Plotly.BUILD);
